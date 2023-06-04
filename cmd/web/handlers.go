81ac2f569c223f4ef944325d8e0a9f05141d56fd
tomas <tomascanepa1397@gmail.com>
2023-06-04 17:21:21 -0300

    application as of chapter 4.9: added model for snippets and updated handlers to use the SnippetModel methods for inserting and retrieving data from the database

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"html/template"
 	"net/http"
 	"strconv"
+
+	//"html/template"
+	"github.com/xtommas/snippetbox/internal/models"
 )
 
 // the (app *application) signature means the functions are defined as a method of the application struct (sort of object oriented)
@@ -15,26 +18,36 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// slice (like an array, but dynamic) of strings for the file names
-	files := []string{
-		"./ui/html/base.html",
-		"./ui/html/partials/nav.html",
-		"./ui/html/pages/home.html",
-	}
-
-	// "files..." unpacks the elements of the slice as individual arguments to the ParseFiles function
-	ts, err := template.ParseFiles(files...)
+	snippets, err := app.snippets.Latest()
 	if err != nil {
-		// use the helpers
 		app.serverError(w, err)
 		return
 	}
 
-	err = ts.ExecuteTemplate(w, "base", nil)
-	if err != nil {
-		// use the helpers
-		app.serverError(w, err)
+	for _, snippet := range snippets {
+		fmt.Fprintf(w, "%+v\n", snippet)
 	}
+
+	// // slice (like an array, but dynamic) of strings for the file names
+	// files := []string{
+	// 	"./ui/html/base.html",
+	// 	"./ui/html/partials/nav.html",
+	// 	"./ui/html/pages/home.html",
+	// }
+
+	// // "files..." unpacks the elements of the slice as individual arguments to the ParseFiles function
+	// ts, err := template.ParseFiles(files...)
+	// if err != nil {
+	// 	// use the helpers
+	// 	app.serverError(w, err)
+	// 	return
+	// }
+
+	// err = ts.ExecuteTemplate(w, "base", nil)
+	// if err != nil {
+	// 	// use the helpers
+	// 	app.serverError(w, err)
+	// }
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
@@ -45,7 +58,19 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+	// Use the SnippetModel Get method to retrieve data for the record by its id, if no matching records are found, return 404 Not Found
+	snippet, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			app.NotFound(w)
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
+
+	// write the snippet data as a plain HTTP response body
+	fmt.Fprintf(w, "%+v", snippet)
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
@@ -56,5 +81,17 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Write([]byte("Create a new snippet..."))
+	// variables holding placeholder data. Remove later
+	title := "O snail"
+	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n- Kobayashi Issa"
+	expires := 7
+
+	// pass the data to the SnippetModel.Insert() method
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
 }
---
+++ internal/models/snippets.go
@@ -0,0 +1,107 @@
+package models
+
+import (
+	"database/sql"
+	"errors"
+	"time"
+)
+
+// Snippet model
+type Snippet struct {
+	Id      int
+	Title   string
+	Content string
+	Created time.Time
+	Expires time.Time
+}
+
+// snippetModel type that wraps the sql.DB connection pool
+type SnippetModel struct {
+	DB *sql.DB
+}
+
+// Insert a snippet in the database
+func (m *SnippetModel) Insert(title string, content string, expires int) (int, error) {
+	// use placeholder parameters (?) to avoid SQL injection
+	query := "INSERT INTO snippets (title, content, created, expires) VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))"
+
+	// Execute the query with the exec method (used for queries that don't return rows)
+	result, err := m.DB.Exec(query, title, content, expires)
+	if err != nil {
+		return 0, err
+	}
+
+	// get the id of the inserted record to return it
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	// convert the id from int4 to int
+	return int(id), nil
+}
+
+// Return a snippet by id
+func (m *SnippetModel) Get(id int) (*Snippet, error) {
+	query := "SELECT id, title, content, created, expires FROM snippets WHERE expires > UTC_TIMESTAMP() AND id = ?"
+
+	// use the QueryRow for queries that return a single row
+	row := m.DB.QueryRow(query, id)
+
+	// initialize a pointer to an empty Snippet struct
+	s := &Snippet{}
+
+	// copy the values from the row to the Snippet object
+	err := row.Scan(&s.Id, &s.Title, &s.Content, &s.Created, &s.Expires)
+	if err != nil {
+		// if the query returns no rows, row.Scan() returns sql.ErrNoRows error
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, ErrNoRecord
+		} else {
+			return nil, err
+		}
+	}
+
+	// return the Snippet object
+	return s, nil
+}
+
+// Return the last 10 snippets
+func (m *SnippetModel) Latest() ([]*Snippet, error) {
+	query := "SELECT id, title, content, created, expires FROM snippets WHERE expires > UTC_TIMESTAMP() ORDER BY id DESC LIMIT 10"
+
+	// use the Query() method for mulitple row responses from db
+	rows, err := m.DB.Query(query)
+	if err != nil {
+		return nil, err
+	}
+
+	// defer to ensure the sql.Rows resultset is properly closed before the Latest() method returns to avoid using all the database pool connections
+	defer rows.Close()
+
+	// empty slice of emtpy Snippet structs
+	snippets := []*Snippet{}
+
+	// use rows.Next to iterate through the rows in the resultset
+	for rows.Next() {
+		// initialize emtpy snippet struct object
+		s := &Snippet{}
+
+		// map it to the row response
+		err = rows.Scan(&s.Id, &s.Title, &s.Content, &s.Created, &s.Expires)
+		if err != nil {
+			return nil, err
+		}
+
+		// add it to the snippets slice
+		snippets = append(snippets, s)
+	}
+
+	// check for errors when the rows.Next() loop finishes
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	// return the snippets slice
+	return snippets, nil
+}

14c7b0ab4eb2a73ad62f0bfca461d8405987d318
tomas <tomascanepa1397@gmail.com>
2023-06-08 17:10:09 -0300

    application as of chapter 6.5: added middleware and middleware chains

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"net/http"
+
+	"github.com/justinas/alice"
 )
 
 // returns a servemux containing the routes of the app
-func (app *application) routes() *http.ServeMux {
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
@@ -15,5 +17,9 @@ func (app *application) routes() *http.ServeMux {
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
-	return mux
+	// create a middleware chain
+	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+
+	// pass the servemux as the 'next' parameter to the middleware
+	return standard.Then(mux)
 }
